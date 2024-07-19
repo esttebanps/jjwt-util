22e75947c9325be3913de612dca6cd453e1e65c8
esttebanps <angelesteban0326gmail.com>
2024-07-19 11:11:26 -0500

    Initial commit

---
+++ src/main/java/com/esttebanps/JwtUtil.java
@@ -0,0 +1,8 @@
+package com.esttebanps;
+
+
+public class JwtUtil {
+   public static Integer sumar(int a, int b) {
+       return a + b;
+   }
+}
---
+++ src/main/java/com/esttebanps/Main.java
@@ -0,0 +1,10 @@
+package com.esttebanps;
+
+import static com.esttebanps.JwtUtil.sumar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(sumar(2,2));
+    }
+}
