8dfc6c4d69c01d83a8eab35bf027c094a900e648
esttebanps <angelesteban0326gmail.com>
2024-07-19 16:04:30 -0500

    Metodos para crear verificar y leer tokens con la biblioteca JJWT

--- src/main/java/com/esttebanps/JwtUtil.java
+++ src/main/java/com/esttebanps/JwtUtil.java
@@ -1,8 +1,90 @@
 package com.esttebanps;
 
 
+import io.jsonwebtoken.*;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 public class JwtUtil {
-   public static Integer sumar(int a, int b) {
-       return a + b;
-   }
+
+    private static final Logger LOGGER = Logger.getLogger(JwtUtil.class.getName());
+
+     /**
+     * Creates a JWT (JSON Web Token) for a given user with specified roles and expiration.
+     * This method generates a token that includes the username as the subject,
+     * roles as claims, and sets an expiration date based on the provided duration.
+     *
+     * @param username     The username of the user for whom the token is being created
+     * @param roles        An array of roles assigned to the user
+     * @param secretKey    The secret key used to sign the JWT
+     * @param durationDays The number of days until the token expires
+     * @return A string representation of the JWT
+     */
+    public static String createToken(String username, String[] roles, SecretKey secretKey, int durationDays) {
+        // Map with roles from user
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", roles);
+        // Duration of the token in milliseconds
+        int days_to_millis = durationDays * 24 * 60 * 60 * 1000;
+        // Token expiration date
+        Date expiration = new Date(System.currentTimeMillis() + days_to_millis);
+        // Create JWT token with the claims and expiration date
+        return Jwts.builder()
+                .signWith(secretKey)
+                .expiration(expiration)
+                .subject(username)
+                .claims(claims)
+                .compact();
+    }
+
+    /**
+     * Validates a JSON Web Token (JWT) using the provided secret key.
+     * This method attempts to parse and verify the JWT using the given secret key.
+     * If the token is valid and not expired, it returns true. Otherwise, it logs
+     * the specific error and returns false.
+     *
+     * @param secretKey The SecretKey used to verify the JWT's signature
+     * @param jwt       The JWT string to be validated
+     * @return          Boolean value indicating whether the token is valid (true) or not (false)
+     */
+    public static Boolean isTokenValid (String jwt, SecretKey secretKey) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(jwt);
+            return true;
+        } catch (ExpiredJwtException e) {
+            LOGGER.severe("token expired");
+        } catch (UnsupportedJwtException e) {
+            LOGGER.severe("token unsupported");
+        } catch (MalformedJwtException e) {
+            LOGGER.severe("token malformed");
+        } catch (IllegalArgumentException e) {
+            LOGGER.severe("illegal args");
+        }
+        return false;
+    }
+
+    public static String getUsernameFromToken(String jwt, SecretKey secretKey) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload().getSubject();
+    }
+
+    public static List getRolesFromToken(String jwt, SecretKey secretKey) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload().get("roles", List.class);
+    }
+
 }
--- src/main/java/com/esttebanps/Main.java
+++ src/main/java/com/esttebanps/Main.java
@@ -1,10 +1,35 @@
 package com.esttebanps;
 
-import static com.esttebanps.JwtUtil.sumar;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+
+import java.util.List;
+
+import static com.esttebanps.JwtUtil.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(sumar(2,2));
+//        System.out.println("--------------- Main ---------------");
+//
+//        String username = "esttebanps";
+//        String[] roles = new String[] { "admin", "user"};
+//        SecretKey secretKey = Jwts.SIG.HS256.key().build();
+//        int duration = 1;
+//
+//        String token = createToken(username, roles, secretKey, duration);
+//        System.out.println("Token creado: " + token);
+//
+//        boolean isValid = isTokenValid(token, secretKey);
+//        System.out.println("Token válido: " + isValid);
+//
+//        String usernameFromToken = getUsernameFromToken(token, secretKey);
+//        System.out.println("Username del token: " + usernameFromToken);
+//
+//        List rolesFromToken = getRolesFromToken(token, secretKey);
+//        System.out.println("Roles del token: " + rolesFromToken);
+
     }
 }
